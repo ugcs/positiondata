4cb9add7655ca5ced239e3e9764a0194a8eebb84
Alexei Yankelevich <ayankelevich@ugcs.com>
2023-12-17 08:56:36 +0100

    update version 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PositionData',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     description='Gereferenced CSV data processing',
     long_description=open('README.md').read(),
