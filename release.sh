88134593e3e7168a08c47590352d632bbae4c212
Alexei Yankelevich <ayankelevich@ugcs.com>
2023-12-17 07:44:15 +0100

    release.sh script and readme.md fixes

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Running tests..."
+./test.sh
+
+# Check if tests were successful
+if [ $? -eq 0 ]; then
+    echo "Tests passed successfully."
+
+    # Extract version from setup.py
+    VERSION=$(python setup.py --version)
+    echo "Extracted version: $VERSION"
+
+    # Tagging the repository
+    echo "Tagging the repository with version $VERSION"
+    git tag -a "v$VERSION" -m "Release version $VERSION"
+
+    # Check if tagging was successful
+    if [ $? -eq 0 ]; then
+        git push origin "v$VERSION"
+
+        # Building the package
+        echo "Building the package..."
+        ./build.sh
+
+        # Check if build was successful
+        if [ $? -eq 0 ]; then
+            # Publishing to PyPI
+            echo "Publishing the package to PyPI..."
+            ./publish.sh
+
+            echo "Release process completed successfully."
+        else
+            echo "Build failed. Release process aborted."
+            exit 1
+        fi
+    else
+        echo "Tagging failed. Release process aborted."
+        exit 1
+    fi
+else
+    echo "Tests failed. Release process aborted."
+    exit 1
+fi
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PositionData',
-    version='0.1.1',
+    version='0.1.3',
     packages=find_packages(),
     description='Gereferenced CSV data processing',
     long_description=open('README.md').read(),
